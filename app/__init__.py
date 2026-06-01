1ecc1497664f8e9d622bd27095559228c4f4f292
mudit0829 <mekasutechnology@gmail.com>
2026-06-01 11:40:57 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,7 @@ def create_app():
         instance_relative_config=True,
         template_folder="../templates",
         static_folder="../static",
-        static_url_path="/static/css",
+        static_url_path="/static",
     )
     app.config.from_object(Config)
 
